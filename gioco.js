2f55d2e25f710a518838fde7ca083d6a4ab877b3
João Moura <joaomdmoura@gmail.com>
2015-04-21 11:14:35 -0400

    updating http to https

--- gioco.js
+++ gioco.js
@@ -3,7 +3,7 @@ var Gioco;
 Gioco = (function() {
   function Gioco(token) {
     this.options = {
-      url: 'http://app.gioco.pro/api/',
+      url: 'https://app.gioco.pro/api/',
       headers: {
         token: window.giocoSettings ? window.giocoSettings.token : token
       }
